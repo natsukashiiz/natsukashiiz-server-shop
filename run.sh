417d93dca3697d517c2aeed1c7451fd80413e08d
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 16:11:56 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ container_name="server-container"
 image_name="server"
 
 # Check if a Docker container with the name "server-container" is running
-if docker ps | grep -q $container_name; then
+if docker ps | grep -q "$container_name"; then
   echo "The "+$container_name+" is already running. Skipping docker-compose."
 else
   # Start Docker containers using docker-compose
