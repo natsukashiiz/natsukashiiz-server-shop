881280466418df6eb022eb13979965604637b838
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 14:37:38 +0700

    update operation point

--- src/main/java/com/natsukashiiz/shop/controller/PointController.java
+++ src/main/java/com/natsukashiiz/shop/controller/PointController.java
@@ -1,6 +1,7 @@
 package com.natsukashiiz.shop.controller;
 
 import com.natsukashiiz.shop.business.PointBusiness;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PointController {
     private final PointBusiness pointBusiness;
 
+    @Operation(summary = "My Point")
     @GetMapping
     public ResponseEntity<?> myPoint() {
         return ResponseEntity.ok(pointBusiness.myPoint());
