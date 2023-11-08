2c6eadaf51f71107843719ca3f37aaae526b9909
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 15:51:46 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-docker-compose up -d
-#sleep 3
+
+# Start Docker containers
+#docker-compose up -d
+
+# Wait for Docker containers to start (optional)
+# sleep 3
+
+# Wait for all background processes (including Docker containers) to finish
 wait
-mvn clean install -DskipTests
-docker build -t server .
-docker run --network natsukashiiz-server-shop_server-net --name server-container -p 8080:8080 -d server
-docker ps
+
+# Build the Maven project
+mvn clean install
+
+# Check if the Maven build was successful or if there were any errors
+if [ $? -eq 0 ]; then
+  echo "Maven build succeeded."
+  # Build a Docker image
+  docker build -t server .
+
+  # Run a Docker container
+  docker run --network natsukashiiz-server-shop_server-net --name server-container -p 8080:8080 -d server
+
+  # Display a list of running Docker containers
+  docker ps
+else
+  echo "Maven build failed."
+fi
