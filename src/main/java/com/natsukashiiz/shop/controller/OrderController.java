b826891473c0114b5cc88325241ae89b4e61320a
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 14:19:34 +0700

    update orders operation

--- src/main/java/com/natsukashiiz/shop/controller/OrderController.java
+++ src/main/java/com/natsukashiiz/shop/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.natsukashiiz.shop.controller;
 
 import com.natsukashiiz.shop.business.OrderBusiness;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
     private final OrderBusiness orderBusiness;
 
+    @Operation(summary = "My Orders")
     @GetMapping
     public ResponseEntity<?> myOrders() {
         return ResponseEntity.ok(orderBusiness.myOrders());
