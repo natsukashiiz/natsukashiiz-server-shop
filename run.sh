b922d8792dc88551d5a268d3ffbcf246eae83b22
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 16:03:27 +0700

    update script

--- run.sh
+++ run.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Start Docker containers
-docker-compose up -d
+# Check if a Docker container with the name "server-container" is running
+if docker ps | grep -q 'server-container'; then
+  echo "The 'server-container' is already running. Skipping docker-compose."
+else
+  # Start Docker containers using docker-compose
+  docker-compose up -d
 
-# Wait for Docker containers to start (optional)
-# sleep 3
-
-# Wait for all background processes (including Docker containers) to finish
-wait
+  # Wait for all background processes (including Docker containers) to finish
+  wait
+fi
 
 # Build the Maven project
 mvn clean install
 
 # Check if the Maven build was successful or if there were any errors
 if [ $? -eq 0 ]; then
+  echo "Maven build succeeded."
   # Build a Docker image
   docker build -t server .
 
