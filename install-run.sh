595b90b830e430772b7f2ff421ab158b52329ac0
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 14:28:36 +0700

    add script

---
+++ install-run.sh
@@ -0,0 +1,7 @@
+docker-compose up -d
+mvn clean install
+docker build -t server .
+docker stop server-container
+docker rm server-container
+docker run --network natsukashiiz-server-shop_server-net --name server-container -p 8080:8080 -d server
+docker ps
