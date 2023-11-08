54270126435c0825b1932446602288d34085d4a5
natsukashiiz <74323401+natsukashiiz@users.noreply.github.com>
2023-11-08 15:46:17 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker-compose up -d
-sleep 5
+#sleep 3
+wait
 mvn clean install -DskipTests
 docker build -t server .
 docker run --network natsukashiiz-server-shop_server-net --name server-container -p 8080:8080 -d server
