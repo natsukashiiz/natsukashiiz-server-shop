d720cfd05aa4ba69404d6957fbdacfad3f129d14
ɴᴀᴛsᴜᴋᴀsʜɪɪᴢ <74323401+natsukashiiz@users.noreply.github.com>
2024-06-30 21:23:29 +0700

    add: version to base entity

--- entity/src/main/java/com/natsukashiiz/shop/entity/BaseEntity.java
+++ entity/src/main/java/com/natsukashiiz/shop/entity/BaseEntity.java
@@ -6,10 +6,7 @@ import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Getter
@@ -20,6 +17,9 @@ public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Version
+    private Long version;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
